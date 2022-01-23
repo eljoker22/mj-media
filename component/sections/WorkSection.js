ec83a9bda7ed753232722f10ac6bd9e5ec0fba66
eljoker22 <eljokermano50@gmail.com>
2022-01-23 21:45:14 +0200

    ediat mobile design

--- component/sections/WorkSection.js
+++ component/sections/WorkSection.js
@@ -1,8 +1,9 @@
 import classes from '../../styles/Home.module.css';
 import Grid from '@mui/material/Grid';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function HowWork() {
+    const mediaQuery = useMediaQuery('(max-width: 410px)');
     const dataCards = [
         {
             title: 'أختر الباقة التى تريدها',
@@ -23,7 +24,7 @@ function HowWork() {
             <p style={{textAlign: 'center', marginBottom: '35px'}}>كل ما عليك هو أختيار الخدمة التى تريدها وأضافة الرابط الخاص بك</p>
             <Grid container spacing={3} style={{alignItems: 'center'}}>
                 {dataCards.map((card) => {
-                    return <Grid key={card.title} item xs={4} sm={4} md={4}>
+                    return <Grid key={card.title} item xs={mediaQuery ? 12 : 4} sm={4} md={4}>
                             <div className={classes.cardHowWork}>
                                 <img src={card.img} alt={card.title} />
                                 <h3>
